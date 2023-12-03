4e67f6a6632830d4dd907cc18064ea56b938681f
Jordan51104 <jb2101554@gmail.com>
2023-12-02 20:56:40 -0600

    day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -39,14 +39,14 @@ impl Round {
 struct Game {
     id: i32,
     rounds: Vec<Round>,
-/*    max_red: Option<i32>,
+    max_red: Option<i32>,
     max_green: Option<i32>,
-    max_blue: Option<i32>*/
+    max_blue: Option<i32>
 }
 
 impl Game {
     fn new(id: i32, rounds: Vec<Round>) -> Game {
-        Game{id, rounds, /*max_red: None, max_blue: None, max_green: None*/}
+        Game{id, rounds, max_red: None, max_blue: None, max_green: None}
     }
 
     fn game_from_str(input: &str) -> Result<Game, &'static str> {
@@ -59,7 +59,7 @@ impl Game {
         Ok(Game::new(id, rounds))
     }
 
-    /*fn find_max(&mut self) {
+    fn find_max(&mut self) {
         let mut max_red = 0;
         let mut max_green = 0;
         let mut max_blue = 0;
@@ -73,7 +73,22 @@ impl Game {
         self.max_red = Some(max_red);
         self.max_blue = Some(max_blue);
         self.max_green = Some(max_green);
-    }*/
+    }
+
+    fn can_get_power(&self) -> bool {
+        if self.max_green.is_some() && self.max_red.is_some() && self.max_blue.is_some() {
+            return true
+        }
+        false
+    }
+
+    fn get_power(&self) -> i32 {
+        if !self.can_get_power() {
+            panic!("Could not get power of max set");
+        } else {
+            return self.max_green.unwrap() * self.max_red.unwrap() * self.max_blue.unwrap()
+        }
+    }
 }
 
 fn main() {
@@ -84,16 +99,12 @@ fn main() {
         .map(|x| Game::game_from_str(x).expect("from_str did not return a Game"))
         .collect::<Vec<_>>();
 
-    let mut valid_game_ids: Vec<i32> = vec![];
+    let mut set_powers: Vec<i32> = vec![];
 
-    for game in games {
-        if game.rounds.iter().all(|x| x.is_valid()) {
-            valid_game_ids.push(game.id)
-        }
+    for mut game in games {
+        game.find_max();
+        set_powers.push(game.get_power());
     }
 
-    println!("{:?}", valid_game_ids);
-    println!("{:?}", valid_game_ids.iter().sum::<i32>());
-    println!("{:?}", valid_game_ids.len());
-
+    println!("{}", set_powers.iter().sum::<i32>())
 }
